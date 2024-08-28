23372b41e017e8da83fadfc85f45b946e5bac05a
Gokulakrishnan62 <gokulakrishnanpalanisamy62@gmail.com>
2024-08-28 22:46:01 +0530

    - add register functionality

---
+++ app/Http/Controllers/Auth/Login.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+
+use App\Http\Controllers\Controller;
+use \App\Http\Requests\Auth\Login as LoginRequest;
+use Illuminate\Support\Facades\Auth;
+
+class Login extends Controller
+{
+    /**
+     * To Login user.
+     *
+     * @param LoginRequest $request
+     * @return \Illuminate\Http\RedirectResponse|string
+     */
+    public function login(LoginRequest $request)
+    {
+        $validated = $request->validated();
+        if (Auth::attempt($validated)) {
+            Auth::login($validated);
+            $request->session()->regenerate();
+            return route('admin.dashboard');
+        } else {
+            return back()->withErrors([
+                'email' => 'The provided credentials do not match our records.',
+            ])->onlyInput('email');
+        }
+    }
+}
---
+++ app/Http/Controllers/Auth/Register.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+
+use App\Http\Controllers\Controller;
+use App\Models\User;
+use App\Http\Requests\Auth\Register as RegisterRequest;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
+
+class Register extends Controller
+{
+    public function register(RegisterRequest $request)
+    {
+        try {
+            $validated = $request->validated();
+            $user = User::create([
+                'name' => $validated['name'],
+                'email' => $validated['email'],
+                'password' => Hash::make($validated['password']),
+            ]);
+
+            Auth::login($user);
+
+            return redirect()->route('admin.dashboard');
+        } catch (QueryException $e) {
+            if ($e->errorInfo[1] == 1062) { // Duplicate entry error code
+                return redirect()->back()->withErrors(['email' => 'The email address is already registered.'])->withInput();
+            }
+            throw $e;
+        }
+    }
+}
